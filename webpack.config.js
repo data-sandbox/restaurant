54160e762830cc9e217ee2d37ad129612b711b12
A <99933422+data-sandbox@users.noreply.github.com>
2023-08-09 15:11:58 -0600

    Test no html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
